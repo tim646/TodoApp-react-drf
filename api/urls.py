9a85a7f5511875c294fc3869d703885655413b16
00011932 <91428417+tim646@users.noreply.github.com>
2022-06-06 12:14:29 +0500

    initial commit

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.apiOverview, name='api-overview'),
+    path('task-list/', views.taskList, name='task-list'),
+    path('task-detail/<int:pk>/', views.taskDetail, name='task-detail'),
+    path('task-create/', views.taskCreate, name='task-create'),
+    path('task-update/<int:pk>/', views.taskUpdate, name='task-update'),
+path('task-delete/<int:pk>/', views.taskDelete, name='task-delete'),
+]
